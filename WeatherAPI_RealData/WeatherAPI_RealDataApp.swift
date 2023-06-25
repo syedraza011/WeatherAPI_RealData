73d43b1fac411aa63b4643610ca4ffb1577ded5d
Syed Raza <syed.raza011@gmail.com>
2023-06-25 12:16:11 -0400

    Initial Commit

---
+++ WeatherAPI_RealData/WeatherAPI_RealDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPI_RealDataApp.swift
+//  WeatherAPI_RealData
+//
+//  Created by Syed Raza on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPI_RealDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

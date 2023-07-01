b61b99a1998bc74c89da71014d5c1b84534744c6
Syed Raza <syed.raza011@gmail.com>
2023-07-01 00:03:38 -0400

    bug fixing

--- WeatherAPI_RealData/Network/WeatherService.swift
+++ WeatherAPI_RealData/Network/WeatherService.swift
@@ -25,29 +25,25 @@ enum APIError: Error {
 class WeatherService {
     
   
-    struct APIconstants {
+    struct constants {
         static let geoBaseURL = "https://api.openweathermap.org/geo/1.0/direct"
         static let weatherBaseURL = "https://api.openweathermap.org/data/2.5/weather"
-        static let APIKey = "efc23031ee630165e1c5a632b7faaccb"
     }
     
     func fetchWeather(lat: Double, lon: Double) async throws -> WeatherResponse {
-        guard let url = URL(string: "\(APIconstants.weatherBaseURL)?lat=\(lat)&lon=\(lon)&appid=\(APIconstants.APIKey)") else { throw APIError.invalidURL("Invalid URL") }
+        guard let url = URL(string: "\(constants.weatherBaseURL)?lat=\(lat)&lon=\(lon)&appid=\(AppInformation.apiKey)") else { throw APIError.invalidURL("Invalid URL") }
         let (data, response) = try await URLSession.shared.data(from: url)
         guard let resp = response as? HTTPURLResponse, resp.statusCode == 200 else {
             throw APIError.invalidResponse("Invalid Response")
         }
         return try JSONDecoder().decode(WeatherResponse.self, from: data)
     } 
-    
-    
-  
     func fetchGeoLocation(_search: String) async throws -> [Location] {
         
      
         let encodedSearch = _search.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         
-        guard let url = URL(string: "\(APIconstants.geoBaseURL)?q=\(encodedSearch)&limit=1&appid=\(APIconstants.APIKey)") else { throw APIError.invalidURL("Invalid URL") }
+        guard let url = URL(string: "\(constants.geoBaseURL)?q=\(encodedSearch)&limit=1&appid=\(AppInformation.apiKey)") else { throw APIError.invalidURL("Invalid URL") }
         let (data, response) = try await URLSession.shared.data(from: url)
         guard let resp = response as? HTTPURLResponse, resp.statusCode == 200 else {
             throw APIError.invalidResponse("Invalid Response")
--- WeatherAPI_RealData/Views/ContentView.swift
+++ WeatherAPI_RealData/Views/ContentView.swift
@@ -8,26 +8,26 @@
 
 import SwiftUI
 struct ContentView: View {
-    
+
     @State var searchText = "" // for the search bar
     @StateObject var ViewModel = WeatherViewModel() // for the weather view
-    
+
     var body: some View {
         NavigationView {
             ZStack {
                 LinearGradient(gradient: Gradient(colors: [.orange, .yellow, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea() // background of the app
-                    
+
                 ZStack {
                     LinearGradient(gradient: Gradient(colors: [.cyan, .cyan, .yellow, .orange]), startPoint: .topLeading, endPoint: .bottomTrailing)
                         .opacity(0.5)
                         .border(.black, width: 0.5)
                         .cornerRadius(25)
                         .frame(maxWidth: 360, maxHeight: 140, alignment: .topLeading)
-                    
-                  
+
+
                     VStack {
-                        
+
                         HStack{ // city, temp
                             Text("\(ViewModel.locationWeather.response.name)")
                                 .font(.system(size: 28))
@@ -36,7 +36,7 @@ struct ContentView: View {
                                 .font(.system(size: 22))
                                 .frame(maxWidth: .infinity, alignment: .trailing)
                         }
-                        
+
                         VStack{
                             HStack { // highs, lows
                                 Text("H: \(formattedTemperature((ViewModel.locationWeather.response.main.temp_max-273.15) * (9/5) + 32))°")
@@ -48,15 +48,15 @@ struct ContentView: View {
                                     .font(.system(size: 20))
                                     .frame(maxWidth: .infinity, alignment: .trailing)
                             }
-                            
+
                         }
                     }
                     .frame(maxWidth: 340, maxHeight: 120, alignment: .topLeading)
                     .background(Color.blue)
-                    
+
                 }
                 .frame(maxWidth: 360, maxHeight: .infinity, alignment: .topLeading)
-                
+
             }
             .navigationTitle(Text("Weather"))
             .searchable(text: $searchText, prompt: "Search for a city/state in USA")
@@ -65,7 +65,7 @@ struct ContentView: View {
             }
         }
     }
-    
+
     private func formattedTemperature(_ temperature: Double) -> String {
         let formatter = NumberFormatter()
         formatter.maximumFractionDigits = 2
@@ -73,17 +73,10 @@ struct ContentView: View {
         return formatter.string(from: NSNumber(value: temperature)) ?? ""
     }
 
-
-                private func convertKelvinToFahrenheit(_ kelvin: Double) -> Int {
-                    Int((kelvin - 273.15) * 9/5 + 32)
-                }
-            
-
-
-
+    private func convertKelvinToFahrenheit(_ kelvin: Double) -> Int {
+            Int((kelvin - 273.15) * 9/5 + 32)
+    }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
